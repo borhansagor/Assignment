adce554d7ee59e99ce57b2c1f3258a2e088000e8
borhansagor <bsagor@student.alamo.edu>
2017-04-29 21:59:58 -0500

    Update Overload.java

--- Overload.java
+++ Overload.java
@@ -1,24 +1,24 @@
 /*Borhan Sagor
 COSC 1336
 overloading methods
-psuedo code: simply going to create a class named OVerload then creating print methods that intake integers and print the average of their sum; overloading them by having the same method modified 
+psuedo code: simply going to create a class named OVerload then creating print methods that intake float and print the average of their sum; overloading them by having the same method modified 
 with different parameters; finally creating a main method that uses the methods.*/
 public class Overload{
 
-    public void average(int pintnumber, int pintnumber2){//intakes two numbers
-        System.out.println("Average is: "+ pintnumber+pintnumber2/2);
+    public void average(float pintnumber, float pintnumber2){//intakes two numbers
+        System.out.println("Average is: "+ (pintnumber+pintnumber2)/2.0);
     }
-    public void average(int pintnumber, int pintnumber2, int pintnumber3){//intakes 3 numbers
-        System.out.println("Average os: "+ pintnumber+pintnumber2+pintnumber3/3);
+    public void average(float pintnumber, float pintnumber2, float pintnumber3){//intakes 3 numbers
+        System.out.println("Average is: "+ (pintnumber+pintnumber2+pintnumber3)/3.0);
     }
-    public void average(int pintnumber, int pintnumber2, int pintnumber3, int pintnumber4 ){//intakes 4 numbers
-        System.out.println("Average is: "+ pintnumber+pintnumber2+pintnumber3+pintnumber4/4);
+    public void average(float pintnumber, float pintnumber2, float pintnumber3, float pintnumber4 ){//intakes 4 numbers
+        System.out.println("Average is: "+ (pintnumber+pintnumber2+pintnumber3+pintnumber4)/4.0);
     }
     public static void main(String[]args){
         Overload objectTest=new Overload();//new obj of class OVerload
-        objectTest.average(5,6);//print method of 2 int avr
-        objectTest.average(5,9,8);//print method of 3 int avr
-        objectTest.average(5,8,9,4);//print method of 4 int avr
+        objectTest.average(5.0f,6.0f);//print method of 2 float avr
+        objectTest.average(5.0f,9.0f,8.0f);//print method of 3 float avr
+        objectTest.average(5.0f,8.0f,9.0f,4.0f);//print method of 4 float avr
         
     }  
 }
